681f34e50021236b9ee4a8989dafa89ac65058ee
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-02-19 20:54:14 -0800

    Add string occurence exercise.

---
+++ Booking/StringOccurence.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Booking
+{
+    [TestClass]
+    public class StringOccurences
+    {
+        [TestMethod]
+        public void Occurence()
+        {
+            const string stream = "hellohellhelhellelloelasnqlhellaoahelelhelloheohlelhoeheohleolhehelhhellooelhhelhhhelloeo.";
+            const string search = "hello";
+
+            int word = 0;
+            string buffer = string.Empty;
+            
+            for (int i = 0, pos = 0; i < stream.Length; i++)
+            {
+                buffer += stream[i];
+
+                if (buffer[pos] != search[pos])
+                {
+                    // Need to come back in the buffer to reprocess the last letter as the begining (hhello)
+                    if (buffer.Length > 1)
+                        i--;
+
+                    // Restart the word position
+                    pos = 0;
+                    buffer = string.Empty;
+                }
+                else
+                    pos++;
+
+                if (buffer != search) continue;
+
+                word++;
+                buffer = string.Empty;
+                pos = 0;
+            }
+
+            Assert.AreEqual(word, 4);
+        }
+    }
+}
