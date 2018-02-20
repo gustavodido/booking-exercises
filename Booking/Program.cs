8e2737bc1619f2d7038aa4650eb568af4cb65808
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-02-19 20:53:04 -0800

    Add project structure.

---
+++ Booking/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intArray = new[] { 1, 101, 10002, 10008, 10, 10, 40, 2, 3, 20, 100, 8, 0 };
+            BubbleSort.Sort(intArray);
+
+            foreach (var i in intArray)
+                Console.Write(i + " ");
+
+            Console.ReadLine();
+        }
+    }
+}
